cb00e2ba6439704e90fc92c96e6e2c4c0dc9c80e
phanduong1232000 <duongptps2000@gmail.com>
2023-01-17 14:08:27 +0700

    fđ

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,73 @@
 new WOW().init();
 
+// $(window).on('load', function () {
+//     $('#myModal').modal('show');
+// });
+
+// $(document).ready(function () {
+//     $(".card-active").click(function () {
+//         // $(".tab").removeClass("active");
+//         $(this).addClass("card");
+//     });
+// });
+
+
+$(document).ready(function () {
+    var toggle = 1;
+    $('.en-up').click(function () {
+        toggle++;
+        if ((toggle % 2) == 0) {
+            $('.card__side--front').css({
+                'transform': 'rotateY(-180deg)'
+            });
+            $('.card__side--back').css({
+                'transform': 'rotateY(0deg)'
+            });
+        } else {
+            $('.card__side--back').css({
+                'transform': 'rotateY(-180deg)'
+            });
+            $('.card__side--front').css({
+                'transform': 'rotateY(0deg)'
+            });
+        }
+    });
+
+    $('.back-prev').click(function () {
+        toggle++;
+        if ((toggle % 2) == 0) {
+            $('.card__side--front').css({
+                'transform': 'rotateY(-180deg)'
+            });
+            $('.card__side--back').css({
+                'transform': 'rotateY(0deg)'
+            });
+        } else {
+            $('.card__side--back').css({
+                'transform': 'rotateY(-180deg)'
+            });
+            $('.card__side--front').css({
+                'transform': 'rotateY(0deg)'
+            });
+        }
+    });
+});
+
 var swiper1 = new Swiper(".swiper-partner", {
     slidesPerView: 4,
     spaceBetween: 10,
     slideToClickedSlide: true,
+    allowTouchMove:false,
     // auto
     // autoplay: {
     //     delay: 3500,
     //     disableOnInteraction: false,
     // },
-    loop: true,
+    loop: false,
     navigation: {
         nextEl: '.next',
         prevEl: '.prev',
-      },
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -21,17 +75,17 @@ var swiper1 = new Swiper(".swiper-partner", {
     breakpoints: {
         // when window width is >= 480px
         120: {
-        slidesPerView: 2,
-        spaceBetween: 2
+            slidesPerView: 2,
+            spaceBetween: 2
         },
         // when window width is >= 640px
         740: {
-        slidesPerView: 3,
-        spaceBetween: 4
+            slidesPerView: 3,
+            spaceBetween: 4
         },
         1323: {
-        slidesPerView: 5,
-        spaceBetween: 1
+            slidesPerView: 5,
+            spaceBetween: 1
         },
         1623: {
             slidesPerView: 6,
@@ -53,7 +107,7 @@ var swiper2 = new Swiper(".swiper-coin", {
     navigation: {
         nextEl: '.next-coin',
         prevEl: '.prev-coin',
-      },
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -61,17 +115,17 @@ var swiper2 = new Swiper(".swiper-coin", {
     breakpoints: {
         // when window width is >= 480px
         120: {
-        slidesPerView: 8,
-        spaceBetween: 2
+            slidesPerView: 8,
+            spaceBetween: 2
         },
         // when window width is >= 640px
         740: {
-        slidesPerView: 15,
-        spaceBetween: 4
+            slidesPerView: 15,
+            spaceBetween: 4
         },
         1323: {
-        slidesPerView: 19,
-        spaceBetween: 1
+            slidesPerView: 19,
+            spaceBetween: 1
         },
         1623: {
             slidesPerView: 22,
